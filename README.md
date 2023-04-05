14d39b3b5efcfe85acdefc37577a1d7d8f4c2d27
Tyler Adams <skipmarcel@gmail.com>
2023-04-04 17:16:24 -0700

    add more extensive error function and finished README

--- README.md
+++ README.md
@@ -13,14 +13,20 @@
 - _eslint_
 - _jest_
 - _babel_
-
-## Description
+- _web API_
 
 ## Setup/Installation Requirements
 
 - _Info regarding API instructions._
+- _To rebuild this project you will need to obtain an API key._
+- _You can go to https://www.exchangerate-api.com/docs/overview_
+- _There you will find instructions for getting your own API key_
+- _Store your API key in your own env file_
+- _The API call should do the rest of the work_
 
 ## Known Bugs
+
+- _Currency pulls are sometimes slow to process_
 
 ## Support and contact details
 
--- src/index.js
+++ src/index.js
@@ -11,5 +11,18 @@ dropdown.addEventListener("click", () => {
       const exchangeRate = data.conversion_rates[selectedCountry];
       rateSpan.textContent = exchangeRate;
     })
-    .catch((error) => console.error(error));
+    .catch(function (error) {
+      const errorMessage =
+        "An error occurred while retrieving exchange rates. Please try again later.";
+      displayError(errorMessage);
+      return error;
+    });
 });
+
+function displayError(errorMessage) {
+  const errorContainer = document.getElementById("error-container");
+  const errorText = document.getElementById("error-text");
+
+  errorText.textContent = errorMessage;
+  errorContainer.style.display = "block";
+}
